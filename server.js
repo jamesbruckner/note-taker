bf66e851168464a7758a90ddc5a6bc3c562274ff
jamesbruckner <jamesbruckner55@gmail.com>
2024-03-18 21:16:37 -0600

    try fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const html_routes = require('./routes/html-routes.js');
 const api_routes = require('./routes/api-routes.js')
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
